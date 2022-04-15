a3e1f7790c188158a94897d3a82017f2139ddd69
w40141 <w.40141@gmail.com>
2022-04-15 18:15:57 +0900

    Success connecting to db

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use sea_orm::Database;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let _ = Database::connect("mysql://root:root@localhost:3306/store").await?;
+    println!("hello");
+    Ok(())
 }
