af53b4cf8b8f7fc64cb29344cb96677f5d784552
w40141 <w.40141@gmail.com>
2022-04-18 14:50:25 +0900

    edit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use dotenv::dotenv;
+use sea_orm::Database;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    dotenv().ok();
+    let database = env::var("DATABASE_URL")?;
+    let _ = Database::connect(database).await?;
+    println!("hello");
+    Ok(())
 }
